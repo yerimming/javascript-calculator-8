d6d1b079540e031ebad0b91f14ad31c1663a4d1d
Kimyerim <yeye092302@gmail.com>
2025-10-20 04:10:55 +0900

    refactor(Calculator): parseInt으로 통합해 커스텀 구분자 처리 및 splitNumbers 메서드 추가
    - 기존 splitByDefaultSeperator를 parseInt으로 통합
    - getCustomSeperator 메서드에서 커스텀 구분자를 추출
    - 기본 구분자(, :)와 커스텀 구분자를 모두 처리하도록 splitNumbers 메서드 구현

--- src/model/Calculator.js
+++ src/model/Calculator.js
@@ -1,7 +1,31 @@
 class Calculator {
-    splitByDefaultSeperator(input) {
-        return input.split(/[,:]/);
+    parseInput(input){
+        let numbersPart = input;
+        const customSeperator = this.getCustomSeperator(input);
+
+        if(customSeperator) {
+            numbersPart = input.split(`\\n`)[1];
+        }
+
+        return this.splitNumbers(numbersPart, customSeperator);
     }
+    getCustomSeperator(input){
+        if(!input.startsWith("//")) return null;
+        const match = input.match(/^\/\/(.*?)\\n/);
+        return match ? match[1] : null;
+    }
+
+    splitNumbers(numbersPart, customSeperator = null) {
+        let delimiters = ",:";
+
+        if(customSeperator) {
+            delimiters += customSeperator;
+        }
+
+        const regex = new RegExp(`[${delimiters}]`);
+        return numbersPart.split(regex);
+    }
+
 }
 
 export default Calculator;

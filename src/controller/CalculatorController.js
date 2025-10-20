aba99e919239cec42f05e3cea4c56180546e748b
Kimyerim <yeye092302@gmail.com>
2025-10-20 18:24:30 +0900

    feat(CalculatorController): Calculator 모델에 입력값을 전달하는 기능 추가
    - 입력받은 문자열을 parseInt 메서드로 전달해 구분자를 기준으로 문자를 구분

--- src/controller/CalculatorController.js
+++ src/controller/CalculatorController.js
@@ -1,12 +1,15 @@
 import ConsoleView from "../view/ConsoleView.js";
+import Calculator from "../model/Calculator.js";
 
 class CalculatorController {
     constructor() {
         this.view = new ConsoleView();
+        this.calculator = new Calculator();
     }
 
     async run() {
         const input = await this.view.getInput();
+        const numbers = this.calculator.parseInput(input);
     }
 }
 

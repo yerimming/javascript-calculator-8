d5e7fd0638bf6c8383a1b1a02e89b21251156516
Kimyerim <yeye092302@gmail.com>
2025-10-20 21:29:26 +0900

    feat(CalculatorController): 계산 흐름 관리하는 기능 추가
    - 입력 문자열 파싱 후 숫자 배열 생성
    - 숫자 유효성 검사 기능 추가
    - 숫자 합산 후 결과 출력
    - try-catch로 예외 처리하여 오류 발생 시 에러 메시지 출력

--- src/controller/CalculatorController.js
+++ src/controller/CalculatorController.js
@@ -1,5 +1,6 @@
 import ConsoleView from "../view/ConsoleView.js";
 import Calculator from "../model/Calculator.js";
+import { Console } from "@woowacourse/mission-utils";
 
 class CalculatorController {
     constructor() {
@@ -8,8 +9,15 @@ class CalculatorController {
     }
 
     async run() {
-        const input = await this.view.getInput();
-        const numbers = this.calculator.parseInput(input);
+        try{
+            const input = await this.view.getInput();
+            const numbers = this.calculator.parseInput(input);
+            this.calculator.validateNumbers(numbers);
+            const result = this.calculator.sum(numbers);
+            this.view.printResult(result);
+        } catch(error) {
+            this.view.printError(error);
+        }
     }
 }
 

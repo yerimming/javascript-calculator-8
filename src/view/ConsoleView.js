0ccf51a79ae6e350a76f0cd59cf3d5c554df0a94
Kimyerim <yeye092302@gmail.com>
2025-10-20 20:53:15 +0900

    feat(ConsoleView): 결과와 에러 메시지를 출력하는 printResult, printError 메서드 추가

--- src/view/ConsoleView.js
+++ src/view/ConsoleView.js
@@ -5,6 +5,14 @@ class ConsoleView {
         const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요\n");
         return input;
     }
+
+    printResult(result) {
+        Console.print(`결과 : ${result}`);
+    }
+
+    printError(error){
+        Console.print(error.message);
+    }
 }
 
 export default ConsoleView;
